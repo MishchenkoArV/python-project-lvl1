5a456b08d3e43abf8b3c765a6b54877dce72118a
Artem Mishchenko <mishchenkoarv@gmail.com>
2022-05-06 09:59:45 +0300

    linter correction

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,8 @@
-#!file <cli.py>
+"""Some module."""
 import prompt
 
 
 def welcome_user():
+    """Get user's name."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
